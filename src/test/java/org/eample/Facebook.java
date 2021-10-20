660aa1e51f7e5eccbe60282445616e3d16e74a7b
Nirmal96 <g.mnirmal96@gmail.com>
2021-10-20 11:39:09 +0530

    Nemo's Updated code

--- src/test/java/org/eample/Facebook.java
+++ src/test/java/org/eample/Facebook.java
@@ -6,5 +6,8 @@ import org.base.BaseClass;
 public class Facebook extends BaseClass{
 public static void main(String[] args) {
 	getbrowser();
+	System.out.println("Nimmu");
+	System.out.println("Nemo");
+	
 }
 }
